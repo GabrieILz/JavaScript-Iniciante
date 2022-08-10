ea6fecc584a0f80a2b6d3165bcbb216f75dab824
GabrieILz <GabrielL.Souza.Ferraz@gmail.com>
2022-08-10 19:54:13 -0300

    Mais coisinhos

---
+++ Tipos_de_Dados/script.js
@@ -0,0 +1,15 @@
+//Boolean
+//Null
+//Undefined
+//Number
+//String
+
+
+var a = "Primeira linha \n Segunda linha";
+var b = 9;
+
+console.log(a);
+console.log(b);
+
+console.log("1" + "1");
+console.log(1 + 1);
